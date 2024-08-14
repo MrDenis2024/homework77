54572d28f6f08aca1b88578944fa60753e28951f
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-14 18:07:00 +0600

    Создана структура в index для запуска сервера

---
+++ guestbook-api/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import messagesRouter from './routes/messages';
+import cors, {CorsOptions} from 'cors';
+
+const app = express();
+const port = 8000;
+
+const whiteList = ['http://localhost:5173'];
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/messages', messagesRouter);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
